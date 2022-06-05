0a152ce143a5b3231372a88dc4bb198edeef0111
reed <edgar.regnault@etu.esisar.grenoble-inp.fr>
2022-06-05 22:23:09 +0200

    .

---
+++ include/movements.h
@@ -0,0 +1,13 @@
+    #ifndef __MOVEMENTS_H__
+    #define __MOVEMENTS_H__
+    #define BOX '$'
+    #define WALL '#'
+    #define PLAYER '@'
+    #define TARGET '.'
+    #define NOTHING ' '
+    #define FULLBOX 'Q'
+    #define OVERTARGET 'O'
+    void move(int *x, int *y, char **currentMap);
+
+
+    #endif
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "keys.h"
+#include "movements.h"
+#include "graphics.h"
+#include "levelLoader.h"
+
+
+int main(void)
+{
+    char *levelFile="level.lvl";
+    
+}
