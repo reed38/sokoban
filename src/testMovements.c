e305254738301fdb302fccc7c671e91ba567e4fc
Estylos <esteban@estylos.fr>
2022-06-07 22:19:38 +0200

    +movements (from test_ed) +keys pour movements

---
+++ src/testMovements.c
@@ -0,0 +1,27 @@
+/**
+ * @file testMovements.c
+ * @author Esteban CADIC, Noé MOREAU, Edgar REGNAULT
+ * @brief Programme de test de la gestion des mouvements du joueur.
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "keys.h"
+#include "graphics.h"
+#include "levelLoader.h"
+#include "movements.h"
+
+void testMovements(void)
+{
+    char *levelFile = "data/level/levels.lvl";
+    char *saveFile = "data/save/save.lvl";
+    readLevelsFile(levelFile);
+    globalCurrentLevel=levelsNode;
+    initLevel(globalCurrentLevel);
+    
+       
+    configureTerminal();
+    interactionLoop(saveFile);
+}
