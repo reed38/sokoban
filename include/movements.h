ee30412643523b35e723953c86cb97ed19c7e7d1
reed <edgar.regnault@etu.esisar.grenoble-inp.fr>
2022-06-07 09:31:34 +0200

    Revert "mouvements prend file en entrée"
    This reverts commit 550b9a553bc59c02f3e64dea400b1df955328dc0.

--- include/movements.h
+++ include/movements.h
@@ -1,12 +1,13 @@
-    #ifndef __MOVEMENTS_H__
-    #define __MOVEMENTS_H__
-    #define BOX '$'
-    #define WALL '#'
-    #define PLAYER '@'
-    #define TARGET '.'
-    #define NOTHING ' '
-    #define FULLBOX 'Q'
-    #define OVERTARGET 'O'
+#ifndef __MOVEMENTS_H__
+#define __MOVEMENTS_H__
+#define BOX '$'
+#define WALL '#'
+#define PLAYER '@'
+#define TARGET '.'
+#define NOTHING ' '
+#define FULLBOX 'Q'
+#define OVERTARGET 'O'
+void move(unsigned int *x, unsigned int *y, char **currentMap);
 
     void move(unsigned int *y, unsigned int *x,  char **currentMap);
 
